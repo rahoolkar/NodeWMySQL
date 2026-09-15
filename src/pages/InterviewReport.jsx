999e347bbeeb1f2eda58e0421754292c44f0c966
rahoolkar <batman30jan2002@gmail.com>
2026-09-16 02:25:31 +0530

    pdf download feature for the users

--- src/pages/InterviewReport.jsx
+++ src/pages/InterviewReport.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const InterviewReport = () => {
+  const { id } = useParams();
+
+  const [report, setReport] = useState(null);
+
+  useEffect(() => {
+    async function fetchReportData() {
+      try {
+        const response = await fetch("http://localhost:8000/report/" + id, {
+          credentials: "include",
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          setReport(data.data);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+
+    fetchReportData();
+  }, []);
+
+  if (!report) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-gray-500 text-lg">Loading Report...</p>
+      </div>
+    );
+  }
+
   return <div>InterviewReport</div>;
 };
 
